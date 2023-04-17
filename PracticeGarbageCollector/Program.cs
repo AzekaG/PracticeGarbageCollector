b1129c861c5246f7d84c9e364fb05fa854f09c26
MatvX_lm73 <MatvX_lm73@student.itstep.org>
2023-04-17 21:23:47 +0300

    Добавьте файлы проекта.

---
+++ PracticeGarbageCollector/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeGarbageCollector
+{
+    internal class Program
+    {
+     
+        class Play: IDisposable
+        {
+            bool isDisposed = false;
+            public string Name { get; set; }
+            public string Genre { get; set; }
+            public string AuthorName { get; set; }
+            public DateTime DateProduction { get; set; }
+
+            public Play() { }
+            public Play(string name, string genre, string authorName, DateTime dateProduction)
+            {
+                Name = name;
+                Genre = genre;
+                AuthorName = authorName;
+                DateProduction = dateProduction;
+            }
+            private void CleaningPlay(bool disposing)
+            {
+                if (!isDisposed)
+                {
+                    if (disposing)
+                        Console.WriteLine("release of managed resources");
+                    Console.WriteLine("Release of Unmanaged resourses");
+                }
+                isDisposed= true;
+            }
+            public void Dispose()
+            {
+                CleaningPlay(true);
+                GC.SuppressFinalize(this);
+            }
+            public void SetInfo()
+            {
+                Console.WriteLine("Enter the Name of Play : ");
+                Name = Console.ReadLine();
+                Console.WriteLine("Enter the Genre of Play : ");
+                Genre = Console.ReadLine();
+                Console.WriteLine("Enter the AuthorName of Play : ");
+                AuthorName = Console.ReadLine();
+                Console.WriteLine("Enter the Date Production of Play : dd.mm.yyyy");
+                DateProduction = DateTime.Parse(Console.ReadLine());
+
+            }
+
+            public void OutputPlay()
+            {
+                Console.WriteLine("Name of Play : "+Name + "\nGenre : "+Genre + "\nAuthor Name : "+AuthorName + "\nDate Prod : "+DateProduction.ToShortDateString());
+            }
+            ~Play() 
+            {
+                Console.WriteLine("Очистка мусора *");
+                CleaningPlay(false);
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a datas : ");
+          using (Play test = new Play())
+            {
+                test.SetInfo();
+                test.OutputPlay();
+            }
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+
+    enum TypeofShop
+    {
+        Products=1,
+        Hardware ,
+        clothing,
+        shoe
+
+    }
+    class Shop : IDisposable
+    {
+       
+        public string NameShop { get; set; }
+        public string AdressShop { get; set; }
+
+        public TypeofShop Type { get; set; }
+        public Shop() { SetInfo(); }
+        public void SetInfo()
+        {
+
+
+            Console.WriteLine("Enter a Name of shop : ");
+            NameShop = Console.ReadLine();
+            Console.WriteLine("Enter a Adress of shop : ");
+            AdressShop = Console.ReadLine();
+            Console.WriteLine("Choose type of shop :  " + "\n1-Product" + "\n2-Hardware" + "\n3 -Clothing " + "\n4-shoe");
+            Type = new TypeofShop();
+            Type = (TypeofShop)int.Parse(Console.ReadLine());
+
+
+        }
+
+
+        bool isDisposed = false;
+        private void Cleaning(bool dispoding)
+        {
+            if (!isDisposed)
+            {
+                if (dispoding)
+                {
+                    Console.WriteLine("Очистка управляемых ресурсов");
+                }
+                Console.WriteLine("Очистка неуправляемых ресурсов");
+            }
+            isDisposed = true;
+
+        }
+        public void OutputInfo()
+        {
+            Console.WriteLine("Name of shop : " + NameShop + "\nType of shop : " + Type + "\nAdress of shop  : " + AdressShop);
+        }
+        public void Dispose() 
+        {
+            Cleaning(true);
+            GC.SuppressFinalize(this);
+        
+        }
+        ~Shop()
+        {
+           
+            Dispose(); 
+        }
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (Shop shop = new Shop()) 
+            {
+                shop.OutputInfo();
+               shop.Dispose();
+            }
+        }
+    }
+}
